972375b17e58bb00d91c76363d5db1f52fd8fea3
MamchenkovaArinaIT13 <dalerkoko5@gmail.com>
2025-12-25 07:53:18 +0300

    Create Validate.java

---
+++ ru.mamchenkova/validatePackage/Validate.java
@@ -0,0 +1,29 @@
+package ru.mamchekova.validatePackage;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Аннотация для валидации классов, указывающая список классов,
+ * которые должны быть проверены при валидации аннотированного класса.
+ * Может применяться как к классам, так и к другим аннотациям.
+ *
+ * @author MamchenkovaArinaIT-13
+ * @version 1.0
+ * @since 2025
+ */
+@Target({ElementType.TYPE,ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Validate {
+
+  /**
+   * Массив классов для валидации.
+   * Определяет список классов, которые должны быть проверены
+   * при обработке аннотированного элемента.
+   *
+   * @return массив классов для валидации
+   */
+  Class<?>[] value();
+}
