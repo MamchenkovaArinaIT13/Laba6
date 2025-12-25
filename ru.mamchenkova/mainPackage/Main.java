9223150deead48add04f6ab98fa3dfb054f993f0
MamchenkovaArinaIT13 <dalerkoko5@gmail.com>
2025-12-25 07:22:52 +0300

    Create Main.java

---
+++ ru.mamchenkova/mainPackage/Main.java
@@ -0,0 +1,25 @@
+package ru.mamchekova.mainPackage;
+
+import ru.mamchekova.menuPackage.MenuAndValidator;
+
+/**
+ * Главный класс приложения, содержащий точку входа в программу.
+ * Инициализирует и запускает систему меню для тестирования функциональности аннотаций.
+ *
+ * @author DalerSafoevIT-13
+ * @version 1.0
+ * @since 2025
+ */
+public class Main {
+
+  /**
+   * Точка входа в приложение. Создает экземпляр класса MenuAndValidator
+   * и запускает основное меню программы.
+   *
+   * @param args аргументы командной строки (не используются).
+   */
+  public static void main(String[] args) {
+    MenuAndValidator menu = new MenuAndValidator();
+    menu.menu();
+  }
+}
