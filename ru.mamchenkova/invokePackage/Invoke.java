87a929f86c718cedbfe143c2b009eae90b7845a6
MamchenkovaArinaIT13 <dalerkoko5@gmail.com>
2025-12-25 07:40:33 +0300

    Create Invoke.java

---
+++ ru.mamchenkova/invokePackage/Invoke.java
@@ -0,0 +1,20 @@
+package ru.mamchekova.invokePackage;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Аннотация для автоматического вызова методов.
+ * Методы, помеченные этой аннотацией, могут быть автоматически
+ * вызваны через механизм рефлексии.
+ *
+ * @author DalerSafoevIT-13
+ * @version 1.0
+ * @since 2025
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Invoke {
+}
